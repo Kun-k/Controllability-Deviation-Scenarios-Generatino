33a949b2ac95f4475d1fd18cf5d9e482f38ccbd1
kun-k <renkun19@foxmail.com>
2023-12-04 21:52:12 +0800

    Change file location of offline dataset.

--- commands.py
+++ commands.py
@@ -9,9 +9,9 @@ def ubuntu_shell(label, argument_list, arg_list):
         else "main_cql.py " if label == "cql" \
         else "main.py"
     commands = []
-    data_root = "../byH2O/dataset/"
+    data_root = "/home1/rk19/Scenario_Generation/dataset/Re_2_H2O/"
     for i in range(len(arg_list)):
-        arg_list[i][6] = data_root + arg_list[i][6]
+        arg_list[i][6] = data_root + arg_list[i][6][int(arg_list[i][3]) - 1]
         cmd_python = python_script + pyfile
         for j in range(len(arg_list[i])):
             cmd_python = cmd_python + argument_list[j] + "=" + arg_list[i][j]
@@ -31,7 +31,7 @@ def ubuntu_shell_1(label, argument_list, arg_list):
     commands = []
     data_root = "/disk/rrkk/scenario_generation/dataset/Re_2_H2O/"
     for i in range(len(arg_list)):
-        arg_list[i][6] = data_root + arg_list[i][6]
+        arg_list[i][6] = data_root + arg_list[i][6][int(arg_list[i][3]) - 1]
         cmd_python = python_script + pyfile
         for j in range(len(arg_list[i])):
             cmd_python = cmd_python + argument_list[j] + "=" + arg_list[i][j]
@@ -47,7 +47,7 @@ def windows_cmd(label, argument_list, arg_list):
         else "main.py"
     data_root = "E:/scenario_generation/dataset/Re_2_H2O/"
     for i in range(len(arg_list)):
-        arg_list[i][6] = data_root + arg_list[i][6]
+        arg_list[i][6] = data_root + arg_list[i][6][int(arg_list[i][3]) - 1]
         cmd = "conda activate scenario_generation && " \
               "E: && " \
               "cd E:/scenario_generation/Controllability-Deviation-Scenarios-Generatino/ && " \
@@ -68,28 +68,53 @@ if __name__ == "__main__":
         "r2-5-2-5",
     ]
 
-    argument_list_sac = [" --USED_wandb", " --ego_policy", " --adv_policy", " --num_agents",
-                         " --r_ego", " --r_adv", " --realdata_path",
-                         " --is_save", " --device", " --seed", " --deviation_theta",
-                         " --alpha_l1"]
-
-    arg_list_sac = [
-        ["True", "sumo", "RL", "3", "r1", "r1", "r3_dis_20_car_4/", "False", "cuda:0", "42", "10", "0"],
-        ["True", "sumo", "RL", "3", "r1", "r1", "r3_dis_20_car_4/", "False", "cuda:0", "42", "10", "1000"],
-        ["True", "sumo", "RL", "3", "r1", "r1", "r3_dis_20_car_4/", "False", "cuda:0", "42", "10", "5000"],
-        # ["True", "sumo", "RL", "3", "r1", "r1", "r3_dis_20_car_4/", "False", "cuda:0", "42", "10", "500"],
-        # ["True", "sumo", "RL", "3", "r1", "r1", "r3_dis_20_car_4/", "False", "cuda:0", "42", "60", "10"],
-        # ["True", "sumo", "RL", "3", "r1", "r1", "r3_dis_20_car_4/", "False", "cuda:0", "42", "60", "100"],
-
-        # ["True", "sumo", "RL", "3", "r1", "r1", "r3_dis_20_car_4/", "False", "cuda:0", "42", "0"],
-        # ["True", "sumo", "RL", "3", "r1", "r1", "r3_dis_20_car_4/", "False", "cuda:0", "42", "10"],
-        # ["True", "sumo", "RL", "3", "r1", "r1", "r3_dis_20_car_4/", "False", "cuda:0", "42", "30"],
-        # ["True", "sumo", "RL", "3", "r1", "r1", "r3_dis_20_car_4/", "False", "cuda:0", "42", "50"],
-        # ["True", "sumo", "RL", "3", "r1", "r1", "r3_dis_20_car_4/", "False", "cuda:0", "42", "70"],
-        # ["True", "sumo", "RL", "3", "r1", "r1", "r3_dis_20_car_4/", "False", "cuda:0", "42", "90"],
+    data_map = [
+        "r3_dis_10_car_2/",
+        "r3_dis_20_car_3/",
+        "r3_dis_20_car_4/",
+        "r3_dis_25_car_5/",
+        "r3_dis_25_car_6/",
+        "r3_dis_25_car_7/"
     ]
 
+    argument_list_sac = [" --USED_wandb", " --ego_policy", " --adv_policy", " --num_agents",
+                         " --r_ego", " --r_adv", " --realdata_path",
+                         " --is_save", " --device", " --seed",
+                         " --deviation_theta", " --alpha_l1", " --batch_ratio"]
+
+    arg_list_sac = [
+        ["True", "sumo", "RL", "3", "r1", "r1", data_map, "False", "cuda:0", "42", "10", "0"],
+        ["True", "sumo", "RL", "3", "r1", "r1", data_map, "False", "cuda:0", "42", "10", "1"],
+        # ["True", "sumo", "RL", "6", "r1", "r1", data_map, "False", "cuda:0", "42", "10", "2", "0"],
+        # ["True", "sumo", "RL", "6", "r1", "r1", data_map, "False", "cuda:0", "42", "10", "100", "0"],
+        # ["True", "sumo", "RL", "6", "r1", "r1", data_map, "False", "cuda:0", "42", "10", "500", "0"],
+        # ["True", "sumo", "RL", "6", "r1", "r1", data_map, "False", "cuda:1", "1000", "10", "0"],
+        # ["True", "sumo", "RL", "6", "r1", "r1", data_map, "False", "cuda:1", "100", "10", "0"],
+        # ["True", "sumo", "RL", "6", "r1", "r1", data_map, "False", "cuda:1", "10", "10", "0"],
+        # ["True", "sumo", "RL", "6", "r1", "r1", data_map, "False", "cuda:1", "1", "10", "0"],
+        # ["True", "sumo", "RL", "6", "r1", "r1", data_map, "False", "cuda:0", "42", "10", "1"],
+        # ["True", "sumo", "RL", "6", "r1", "r1", data_map, "False", "cuda:0", "42", "10", "2"],
+        # ["True", "sumo", "RL", "6", "r1", "r1", data_map, "False", "cuda:0", "42", "10", "5"],
+        # ["True", "sumo", "RL", "6", "r1", "r1", data_map, "False", "cuda:0", "42", "10", "10"],
+        # ["True", "sumo", "RL", "6", "r1", "r1", data_map, "False", "cuda:0", "42", "10", "100"],
+        # ["True", "sumo", "RL", "6", "r1", "r1", data_map, "False", "cuda:0", "42", "10", "500"],
+        # ["True", "sumo", "RL", "6", "r1", "r1", data_map, "False", "cuda:0", "42", "10", "1000"],
+    ]
+
+    argument_list_cql = [" --USED_wandb", " --ego_policy", " --adv_policy", " --num_agents",
+                         " --r_ego", " --r_adv", " --realdata_path",
+                         " --is_save", " --device", " --seed", " --alpha_l2"]
+
+    arg_list_cql = [
+        ["True", "sumo", "RL", "3", "r1", "r1", data_map, "False", "cuda:0", "42", "0"],
+        ["True", "sumo", "RL", "3", "r1", "r1", data_map, "False", "cuda:0", "42", "1"],
+        ["True", "sumo", "RL", "3", "r1", "r1", data_map, "False", "cuda:0", "42", "5"],
+        ["True", "sumo", "RL", "3", "r1", "r1", data_map, "False", "cuda:0", "42", "10"],
+        ["True", "sumo", "RL", "3", "r1", "r1", data_map, "False", "cuda:0", "42", "100"],
+        ["True", "sumo", "RL", "3", "r1", "r1", data_map, "False", "cuda:0", "42", "1000"],
+    ]
     # ubuntu_shell("sac", argument_list_sac, arg_list_sac)
+    # ubuntu_shell("cql", argument_list_cql, arg_list_cql)
     # ubuntu_shell("", argument_list, arg_list)
     windows_cmd("sac", argument_list_sac, arg_list_sac)
 
