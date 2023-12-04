b8f258b0de2eddac5fe20171a2b08bcf86d9b395
kun-k <renkun19@foxmail.com>
2023-12-04 22:09:42 +0800

    Add some parameters.

--- buffer/sampler.py
+++ buffer/sampler.py
@@ -8,7 +8,7 @@ class StepSampler(object):
         self._env = env
         self._traj_steps = 0
 
-    def sample(self, ego_policy, adv_policy, n_steps, deterministic=False,
+    def sample(self, ego_policy=None, adv_policy=None, n_steps=100, deterministic=False,
                replay_buffer_ego=None, replay_buffer_adv=None, joint_noise_std=0.):
         observations = []
         actions_ego = []
@@ -69,14 +69,6 @@ class StepSampler(object):
                 self._current_observation = self.env.reset()
         self.env.traci_close()
 
-        # return dict(
-        #     observations=np.array(observations, dtype=np.float32),
-        #     actions=np.array(actions, dtype=np.float32),
-        #     rewards=np.array(rewards, dtype=np.float32),
-        #     next_observations=np.array(next_observations, dtype=np.float32),
-        #     dones=np.array(dones, dtype=np.float32),
-        # )
-
     @property
     def env(self):
         return self._env
@@ -89,7 +81,7 @@ class TrajSampler(object):
         self._env = env
         self.rootsavepath = rootsavepath
 
-    def sample(self, ego_policy, adv_policy, n_trajs, scenario=None, deterministic=False,
+    def sample(self, ego_policy=None, adv_policy=None, n_trajs=100, scenario=None, deterministic=False,
                replay_buffer_ego=None, replay_buffer_adv=None):
         global info, done
         trajs = []
@@ -105,11 +97,6 @@ class TrajSampler(object):
 
             num_av_crash = 0
             num_bv_crash = 0
-            # ego_col_cost = []
-            # adv_col_cost = []
-            # adv_road_cost = []
-            # rewards_ego_col = []
-            # rewards_ego_speed = []
 
             observation = self.env.reset(scenario)
             for _ in range(self.max_traj_length):
@@ -134,11 +121,6 @@ class TrajSampler(object):
                 rewards_adv.append(reward[1])
                 dones.append(done)
                 next_observations.append(next_observation)
-                # ego_col_cost.append(info[1][0])
-                # adv_col_cost.append(info[1][1])
-                # adv_road_cost.append(info[1][2])
-                # rewards_ego_col.append(info[1][5])
-                # rewards_ego_speed.append(info[1][6])
 
                 if replay_buffer_ego is not None:
                     replay_buffer_ego.add_sample(
@@ -186,13 +168,6 @@ class TrajSampler(object):
                 traj_dis=traj_dis,
                 collision_time=collision_time,
                 collision_dis=collision_dis,
-                # dis_av_crash=info[1][3],
-                # dis_bv_crash=info[1][4],
-                # ego_col_cost=np.array(ego_col_cost, dtype=np.float32),
-                # adv_col_cost=np.array(adv_col_cost, dtype=np.float32),
-                # adv_road_cost=np.array(adv_road_cost, dtype=np.float32),
-                # rewards_ego_col=np.array(rewards_ego_col, dtype=np.float32),
-                # rewards_ego_speed=np.array(rewards_ego_speed, dtype=np.float32),
             ))
         self.env.traci_close()
 
